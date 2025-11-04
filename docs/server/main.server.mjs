2e4eb0f6589a8e38a3bf3cfb273c649d52c4edef
saravanan <saravanans2496@gmail.com>
2025-11-04 23:39:42 +0530

    Deploy Angular app

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-U5REOTDZ.mjs";import"./chunk-WCTRKZN5.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-GARNATNQ.mjs";import"./chunk-WCTRKZN5.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
